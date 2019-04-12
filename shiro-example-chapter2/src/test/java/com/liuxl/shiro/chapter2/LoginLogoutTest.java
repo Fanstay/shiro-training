b89503119223e2d88be225d7675d00a8c5222293
823810399 <823810399@qq.com>
2019-04-12 23:09:14 +0800

    增加多个realm的测试用例testCustomMultiRealm

--- shiro-example-chapter2/src/test/java/com/liuxl/shiro/chapter2/LoginLogoutTest.java
+++ shiro-example-chapter2/src/test/java/com/liuxl/shiro/chapter2/LoginLogoutTest.java
@@ -71,4 +71,35 @@ public class LoginLogoutTest {
         //6、退出
         subject.logout();
     }
+
+    /**
+     * 多Realm配置
+     */
+    @Test
+    public void testCustomMultiRealm() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-multi-realm.ini");
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("wang", "123");
+
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+            System.out.println("登录成功");
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            //5、身份验证失败
+            System.out.println("登录失败");
+        }
+
+        //Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+    }
 }
---
+++ shiro-example-chapter2/src/test/java/com/liuxl/shiro/chapter2/realm/MyRealm2.java
@@ -0,0 +1,37 @@
+package com.liuxl.shiro.chapter2.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @Author: liuxl
+ * @Date: 2019/4/12 22:41
+ * @Version 1.0
+ */
+public class MyRealm2 implements Realm {
+
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        //仅支持UsernamePasswordToken类型的Token
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = (String)authenticationToken.getPrincipal();//得到用户名
+        String password = new String((char[])authenticationToken.getCredentials());//得到密码
+        if(!"wang".equals(username)){
+            throw new UnknownAccountException();//如果用户名错误
+        }
+        if(!"123".equals(password)){
+            throw new IncorrectCredentialsException();//密码错误
+        }
+        //如果身份认证成功，返回一个AuthenticationInfo实现
+        return new SimpleAuthenticationInfo(username,password,getName());
+    }
+}
